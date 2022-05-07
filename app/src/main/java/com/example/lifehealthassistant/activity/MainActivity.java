87095427178120004f336c58c59d4912c32924bc
lzn <482087021@qq.com>
2022-05-07 13:08:19 +0800

    完成首页

--- app/src/main/java/com/example/lifehealthassistant/activity/MainActivity.java
+++ app/src/main/java/com/example/lifehealthassistant/activity/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.lifehealthassistant.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
 
 import com.example.lifehealthassistant.R;
 
@@ -12,5 +17,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WebView webView = (WebView) findViewById(R.id.web_view);
+        webView.getSettings ( ).setJavaScriptEnabled(true) ;
+        webView.setWebViewClient ( new WebViewClient() );
+        webView.loadUrl("https://jiankang.163.com") ;
+
     }
+
+    public void onEpidemic(View v){
+        Intent intent = new Intent(MainActivity.this,EpidemicActivity.class);
+        startActivity(intent);
+    }
+
+    public void onDiet(View v){
+        Intent intent = new Intent(MainActivity.this,DietActivity.class);
+        startActivity(intent);
+    }
+
+    public void onExercise(View v){
+        Intent intent = new Intent(MainActivity.this,ExerciseActivity.class);
+        startActivity(intent);
+    }
+    public void onMain(View v){
+        Intent intent = new Intent(MainActivity.this,MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void onMedical(View v){
+        Intent intent = new Intent(MainActivity.this,MedicalActivity.class);
+        startActivity(intent);
+    }
+
+    public void onPerson(View v){
+        Intent intent = new Intent(MainActivity.this,UserActivity.class);
+        startActivity(intent);
+    }
+
 }
