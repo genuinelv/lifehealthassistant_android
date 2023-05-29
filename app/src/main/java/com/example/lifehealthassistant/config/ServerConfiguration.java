a21cbe693c7800b5d90f7d2468d36d30386062df
lzn <482087021@qq.com>
2023-05-29 17:45:01 +0800

    修改服务器地址及端口号

--- app/src/main/java/com/example/lifehealthassistant/config/ServerConfiguration.java
+++ app/src/main/java/com/example/lifehealthassistant/config/ServerConfiguration.java
@@ -5,6 +5,6 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public class ServerConfiguration {
-    public static final String IP = "http://192.168.201.71/";//服务器地址，每次换网则修改
+    public static final String IP = "http://10.135.62.18:10010/";//服务器地址，每次换网则修改
 
 }
