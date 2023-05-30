0f8c9c8d29282f61e286904155d660f42b502ff9
lzn <482087021@qq.com>
2023-05-30 11:07:47 +0800

    .

--- app/src/main/java/com/example/lifehealthassistant/config/ServerConfiguration.java
+++ app/src/main/java/com/example/lifehealthassistant/config/ServerConfiguration.java
@@ -5,6 +5,6 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public class ServerConfiguration {
-    public static final String IP = "http://10.135.62.18:10010/";//服务器地址，每次换网则修改
+    public static final String IP = "http://1.1.1.1:10010/";//服务器地址，每次换网则修改
 
 }
